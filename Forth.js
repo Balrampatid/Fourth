7ca374ab4d5f4e3fd5a817df0dffbc556b3a1cc1
Balram Patidar <balram421021@gmail.com>
2025-04-12 11:27:01 +0530

    Add files via upload

---
+++ Forth.js
@@ -0,0 +1,50 @@
+function add(a,b)
+{
+    var c=a+b;
+    console.log("Addition is "+c);
+}
+function sub(a,b)
+{
+    var c=a-b;
+    console.log("Subtraction  is "+c);
+}
+function hello(a,b)
+{
+
+    console.log("welcome to the Sky ");
+    add(a,b);
+    sub(a,b);
+    console.log("Thankyou");
+}
+hello(30,20);
+
+// when we run both function add and sub then we go for this process 
+/*function add(a,b)
+{
+    var c=a+b;
+    console.log("Addition is "+c);
+}
+function sub(a,b)
+{
+    var c=a-b;
+    console.log("subtraction is "+c);
+}
+function hello(a,b,operation)
+{
+
+    console.log("welcome to the Sky ");
+    if(operation==1)
+    {
+        add(a,b);
+    }
+    if(operation==2)
+    {
+        sub(a,b);
+    }
+    console.log("Thankyou");
+}
+hello(10,20,1);//one for use addition 
+hello(30,10,2);//2 for subtaction 
+*/
+//in this code have an issue no we will be remaiber like 1 for add 2
+// for sub .....so resole this problem on fifth.js 
